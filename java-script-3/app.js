8be02ce382172c272d392a982d20c11273cf1c8a
Sameer Hanif <141715583+sameerhanif959@users.noreply.github.com>
2024-03-10 11:27:29 +0500

    Add files via upload

---
+++ java-script-3/app.js
@@ -0,0 +1,391 @@
+              //----------------------------   CHAPTER NO 9 ------------------------------- //
+
+                                //   Question N0 1  //
+
+                                // START //
+
+// var cityName = prompt("Enter the city name:");
+
+// if (cityName.toLowerCase() === "karachi") {
+
+//     console.log("Welcome to the city of lights");
+// } else {
+    
+//     console.log("Welcome to " + cityName);
+// }
+
+                                // END //
+
+                                 //   Question N0 2  //                                 
+
+                                // START //
+
+                               
+// var gender = prompt("Enter your gender (male/female):");
+
+
+// gender = gender.toLowerCase();
+
+
+// if (gender === "male") {
+//     console.log("Good Morning Sir.");
+// } else if (gender === "female") {
+//     console.log("Good Morning Ma'am.");
+// } else {
+//     console.log("Invalid gender entered. Please enter 'male' or 'female'.");
+// }
+
+
+
+
+                                   // END //
+
+                                       //   Question N0 3  //    
+
+                                        
+                                   // START //
+
+                
+// var color = prompt("Enter the color of the road traffic signal (red/yellow/green):");
+
+
+// color = color.toLowerCase();
+
+
+// switch (color) {
+//     case "red":
+//         console.log("Vehicles must stop.");
+//         break;
+//     case "yellow":
+//         console.log("Vehicles should get ready to move.");
+//         break;
+//     case "green":
+//         console.log("Vehicles can move.");
+//         break;
+//     default:
+//         console.log("Invalid color entered. Please enter 'red', 'yellow', or 'green'.");
+//         break;
+// }
+
+
+                                   
+                                   // END //
+
+                                   
+                                       //   Question N0 4  //
+
+                                           // START //
+
+
+
+
+// var remainingFuel = parseFloat(prompt("Enter the remaining fuel in your car (in liters):"));
+
+// // C
+// if (remainingFuel < 0.25) {
+//     console.log("Please refill the fuel in your car.");
+// } else {
+//     console.log("You have enough fuel in your car.");
+// }
+ 
+                                                   // END //
+
+                                                    //   Question N0  5 //
+
+                                                // START //
+                                                // var a = 4;
+                                                // if (++a === 5){
+                                                //     alert("given condition for variable a is true");
+                                                // }
+
+                                                
+
+
+                                                // END //
+
+
+                                        //    Question no 6 //
+
+                                        // START //
+
+                                        // var english = 100;
+                                        // urdu = 100;
+                                        // maths = 100;
+                                        // totalMarks= english + urdu + maths;
+                                        // mathMarks = +prompt("Enter your Maths Marks");
+                                        // urduMarks = +prompt("Enter your Urdu Marks");
+                                        // englishMarks = +prompt("ENter your English Marks");
+                                        // obtainMarks = mathMarks + urduMarks + englishMarks;
+                                        // percentage = ((obtainMarks / totalMarks) * 100);
+                                        // if(percentage >= 80)
+                                        // {document.write("<h1>Marks Sheet</h1>"  + "Total marks :" + "  " + totalMarks + "</br>" + "Marks obtain :"+ "  " +obtainMarks + "</br>" + "Percentage :"+ "  " + percentage +"%" +"</br>"+ "Grade :"+ "  " +"A-one"+"</br>"+"Remarks :  You need to improve");}
+                                        // else if(percentage >= 70)
+                                        // {document.write("<h1>Marks Sheet</h1>"  + "Total marks :" + "  " + totalMarks + "</br>" + "Marks obtain :"+ "  " +obtainMarks + "</br>" + "Percentage :"+ "  " + percentage +"%" +"</br>"+ "Grade :"+ "  " +"A"+"</br>"+"Remarks :  Good");}
+                                        // else if(percentage >= 60)
+                                        // {document.write("<h1>Marks Sheet</h1>"  + "Total marks :" + "  " + totalMarks + "</br>" + "Marks obtain :"+ "  " +obtainMarks + "</br>" + "Percentage :"+ "  " + percentage +"%" +"</br>"+ "Grade :"+ "  " +"B"+"</br>"+"Remarks :  You need to improve");}
+                                        // else{document.write("<h1>Marks Sheet</h1>"  + "Total marks :" + "  " + totalMarks + "</br>" + "Marks obtain :"+ "  " +obtainMarks + "</br>" + "Percentage :"+ "  " + percentage +"%" +"</br>"+ "Grade :"+ "  " +"Fail"+"</br>"+"Remarks :  Sorry");}  
+
+
+
+                                    // END //
+
+                                    //    Question no 7 //
+
+                                    // START //
+                                    // var secratNumer = 7;
+                                    // userNumber = +prompt("Guess Game \nGuess the number  from 1 to 10 & get rewarded");
+                            
+                                    // if(userNumber == secratNumer)
+                                    // {document.write("<h1>Bingo! Correct answer </h1> <h2>Hamdard ki motor bike apki hoi</h2></p>");}
+                                    // else if(userNumber == secratNumer +1 || userNumber == secratNumer -1)
+                                    // {document.write("<h1>Close enough to the correct answer</h1>");}
+                                    // else{ document.write("<h1>Ohoo!!</h1> \n <h2>Better luck next time</h2>");}
+
+                                    // END //
+  
+                                                //    Question no 8 //
+
+                                                //   START //
+
+        // var divNumber = 3;
+        // userNumber = +prompt("Check your number is divisible by 3 or not!!")
+        // modulus = userNumber % divNumber;
+        // if(modulus === 0)
+        // {document.write("<h1>Division is posible</h1>");}
+        // else{ document.write("<h1> Division not posible </h1>");}
+
+                                                // END //
+
+                                                     //    Question no 9 //
+
+                                                    //  START //
+
+        // var userNumber = +prompt("Check your number is even or odd!!");
+
+        // if (userNumber % 2 === 0)
+        // {document.write("<h1>Your given number" + " " + userNumber + " " + "is even </h1>");}
+        // else 
+        // {document.write("<h1> Your given number" + " " + userNumber + " " + "is odd</h1>");}
+
+                                                    // END //
+
+
+                                                      //    Question no 10 //
+
+                                                    //   START //
+    //       var userInput = +prompt("Enter your current city temperature");
+       
+    //      if (userInput >= 40)
+    //    { document.write("<h1>It is too hot outside.</h1>");}
+    //          else if(userInput >= 30)
+    //            { document.write("<h1>The Weather today is Normal.</h1>");}
+    //               else if(userInput >= 20)
+    //              { document.write("<h1>Today’s Weather is cool.</h1>");}
+    //      else if (userInput >= 10)
+    //              { document.write("<h1>OMG! Today’s weather is so Cool.</h1>");}
+    //                   else {document.write("<h1>It's freezing outside.</h1>");}
+
+                                                    // END //
+
+                                                    //    Question no 11 //
+
+                                                    // START //
+    // var firstNumber = +prompt("Enter first value");
+    //  operation = prompt("Enter operation symbol example : +  - /  *  %");
+    //   secondNumber = +prompt("Enter Second value");  
+
+    //      if(operation === "/" )
+    //         {document.write("<h1>The calculation result is"+" "+firstNumber+" "+ "/" +" " +secondNumber +" " +"=" + " " +(firstNumber / secondNumber) + "</h1>");}
+
+
+    //                else if (operation === "+" )
+
+    //                {document.write("<h1>The calculation result is"+" "+firstNumber+" "+ "+" +" " +secondNumber +" " +"=" + " " +(firstNumber + secondNumber) + "</h1>");}
+
+    //                  else if (operation === "-" )
+    //             {document.write("<h1>The calculation result is"+" "+firstNumber+" "+ "-" +" " +secondNumber +" " +"=" + " " + (firstNumber - secondNumber) + "</h1>");}
+
+    //              else if (operation === "*" )
+    //                     {document.write("<h1>The calculation result is"+" "+firstNumber+" "+ "*" +" " +secondNumber +" " +"=" + " "+(firstNumber * secondNumber) + "</h1>");}
+
+
+    //                  else if (operation === "%" )
+    //                   {document.write("<h1>The calculation Persentage </br> 1st number"+" "+firstNumber+" "+ "from"+" "+"2nd number"+" "+secondNumber +"</br>" +"=" + " "+(firstNumber / secondNumber * 100) + "%" + "</h1>");}
+
+
+    //            else{document.write("<h1>Kindly enter a valid value</h1>");}               
+                                                    // END //
+
+
+
+
+
+                    //----------------------------   CHAPTER NO 12 ------------------------------- //
+
+
+                    // Question no 1 //
+
+                    // START //
+
+                    // ????????????????????????????????  //
+
+                    // END //
+
+
+                    // Question no 2 //
+
+                    // START //
+
+
+        //             var int_one = +prompt("Enter your 1st integer");
+        //   var int_two = +prompt("Enter your 2nd integer");
+          
+        //   if(int_one == int_two)
+        //   {document.write("<h1>Both integers are equal</h1>");}
+        //   else if (int_one > int_two)
+        //   { document.write("<h1>The larger value is"+" "+ int_one +"</h1>");}
+        //   else if (int_two > int_one)
+        //   { document.write("<h1>The larger value is"+" "+ int_two+"</h1>");}
+        //   else{"Error"}
+
+
+                    // END //
+
+                     // Question no 3 //
+
+                    //  START //
+
+                    // var userInput = +prompt("Enter a number");
+
+                    // if(userInput >=1)
+                    // {document.write("<h1>Number is positive</h1>");}
+                    // else if (userInput === 0)
+                    // {document.write("<h1>Number is Zero</h1>");}
+                    // else{ document.write("<h1>Number is Neative</h1>");}
+
+                    // END //
+
+
+                                 // Question no 4 //
+
+                                //  START //
+
+                    // var character = prompt("Write a alphbet \n to find it is vowel or not");
+
+        //   if(character === "a")
+        //   {document.write("<h1> True" +" " + character + " " + "is a vowel</h1>");}
+        //   if(character === "e")
+        //   {document.write("<h1> True" +" " + character + " " + "is a vowel</h1>");}
+        //   if(character === "i")
+        //   {document.write("<h1> True" +" " + character + " " + "is a vowel</h1>");}
+        //   if(character === "o")
+        //   {document.write("<h1> True" +" " + character + " " + "is a vowel</h1>");}
+        //   if(character === "u")
+        //   {document.write("<h1> True" +" " + character + " " + "is a vowel</h1>");}
+        //   else{document.write("<h1> False" +" " + character + " " + "is not a vowel</h1>");}
+
+
+                                           // END //
+
+
+
+                                                 // Question no 5 //
+
+                                                //  START //
+
+        //                                    var correct_password = 54321;
+        //   user_password = +prompt("Enter Your Pasword");
+          
+        //   if(user_password === correct_password)
+        //   {alert("Your password is accepted");  document.write("<h1> Welcome to Saylani Smit</h1>");}
+        //   else{alert("You have enter an in-valid password");}
+
+                            //   END //
+
+
+                              // Question no 6 //
+
+                            //   START //
+
+                            var hour = 13;
+                            if (hour < 18) {greeting = "Good day";}
+                            else { greeting = "Good evening";}
+                          
+                                 var greeting;
+                                 var hour = 13;
+                                 if(hour < 18)
+                                 {alert(greeting = "Good day");}
+                                 else { alert(greeting = "Good evening");}
+
+                                            //    END //
+
+
+                                              // Question no 7 //
+
+                                            //   STARRT //
+                 var time = +prompt("Enter Time (like = 19:00)");
+
+                if (time === "13:00") 
+                {alert("it's 1pm");}
+                else if (time === "14:00") 
+                {alert("it's 2pm");}
+                else if (time === "15:00")
+                {alert("it's 3pm");}
+                else if (time === "16:00")
+                {alert("it's 4pm");}
+                 else if (time === "17:00")
+                {alert("it's 5pm");}
+                else if (time === "18:00")
+                 {alert("it's 6pm");}
+                else if (time === "19:00")
+                {alert("it's 7pm");}
+                else if (time === "20:00")
+                {alert("it's 8pm");}
+                else if (time === "21:00")
+                {alert("it's 9pm");}
+                else if (time === "22:00")
+                {alert("it's 10pm");}
+                 else if (time === "23:00")
+                {alert("it's 11pm");}
+                 else if (time === "24:00")
+                {alert("it's 12pm");}
+                 else{alert("Enter a valid time")}
+                                            // END //
+
+
+                                            
+
+
+                       
+
+
+                                                      
+                                                    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
